daf5040d544f4835a5fdf6b15c7520307c9c0b1c
khovan123 <minhpnq1807@gmail.com>
2026-07-09 23:23:38 +0700

    hotfix: compute service prisma --project id

--- prisma.compute.ts
+++ prisma.compute.ts
@@ -2,8 +2,14 @@ import { defineComputeConfig } from "@prisma/compute-sdk/config";
 
 export default defineComputeConfig({
   app: {
-    name: "lcsp",
+    name: "@lcsp/api",
     root: "apps/api",
-    framework: "nestjs",
+    framework: "custom",
+    httpPort: 3000,
+    build: {
+      command: null,
+      outputDirectory: "dist",
+      entrypoint: "src/main.js",
+    },
   },
 });
