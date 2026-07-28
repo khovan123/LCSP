3857bcd6c4493d49e6637c06c1e7e07c7b788ad7
nhibao08 <lebaonhi0805@gmail.com>
2026-07-28 15:25:00 +0700

    fix(prisma): LCSP-63 prisma deploy

--- prisma.compute.ts
+++ prisma.compute.ts
@@ -1,10 +1,14 @@
 import { defineComputeConfig } from "@prisma/compute-sdk/config";
 
 export default defineComputeConfig({
-  app: {
-    name: "@lcsp/api",
-    root: "apps/api",
-    framework: "nestjs",
-    httpPort: 3000,
+  apps: {
+    api: {
+      root: "apps/api",
+      framework: "nestjs",
+    },
+    web: {
+      root: "apps/web",
+      framework: "nextjs",
+    },
   },
 });
